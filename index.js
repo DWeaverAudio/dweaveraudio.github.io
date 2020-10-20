7d7a612616284948bbc6a82ce636cbea5fe37d9c
ScowlingOwlSound <develop@weaveraudio.com>
2020-10-20 18:22:52 +1100

    dd Config for ignore
    :#

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json');
 
-const token = process.env.TOKEN;
+const token = config.token;
 
 client.login(token)
 
