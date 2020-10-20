b6a7dbdd1d8bcdfdd345c70f43b14d0b742cbb3d
ScowlingOwlSound <develop@weaveraudio.com>
2020-10-20 18:31:31 +1100

    Index.js typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.js');
+const config = require('./config.json');
 
 const token = config.token;
 
